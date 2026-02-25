e535b6ae9675d8ac2411f154003f08fe5ef7164c
kapinarc <89167555+KadalK@users.noreply.github.com>
2026-02-25 14:54:02 +0100

    get a good main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -16,7 +16,7 @@ int main()
 	try
 	{
 		pingouin->setGrade(5);
-		form = randIntern.makeForm("robotomy request", "");
+		form = randIntern.makeForm("robotomy request", "Sarah Connor");
 		std::cout << *pingouin << std::endl;
 		std::cout << *form << std::endl;
 		pingouin->signAForm(form);
